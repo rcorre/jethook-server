b76c1cbb1602eb93aa39bf5459528c089887d71b
Ryan Roden-Corrent <ryan@rcorre.net>
2020-11-25 18:29:09 -0500

    Implement basic lobby handling

--- main.go
+++ main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"path"
 )
+
+// name -> address
+var lobbies map[string]string
+
+func v1LobbyList(r *http.Request) ([]byte, int) {
+	data, err := json.Marshal(lobbies)
+	if err != nil {
+		return nil, http.StatusInternalServerError
+	}
+	return data, http.StatusOK
+}
+
+func v1LobbyPut(r *http.Request) ([]byte, int) {
+	name := path.Base(r.URL.Path)
+
+	for n, address := range lobbies {
+		if n == name || address == r.RemoteAddr {
+			return nil, http.StatusConflict
+		}
+	}
+
+	lobbies[name] = r.RemoteAddr
+	return nil, http.StatusOK
+}
+
+func v1LobbyDelete(r *http.Request) ([]byte, int) {
+	name := path.Base(r.URL.Path)
+
+	if address, ok := lobbies[name]; !ok {
+		return nil, http.StatusNotFound
+	} else if address != r.RemoteAddr {
+		return nil, http.StatusNotFound
+	}
+
+	delete(lobbies, name)
+	return nil, http.StatusOK
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -13,8 +51,32 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	http.HandleFunc("/v1/lobby", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello: %+v", r.Header)
+
+	lobbies = map[string]string{}
+
+	http.HandleFunc("/v1/lobby/", func(w http.ResponseWriter, r *http.Request) {
+		var body []byte
+		var code int
+
+		switch r.Method {
+		case http.MethodGet:
+			body, code = v1LobbyList(r)
+		case http.MethodPut:
+			body, code = v1LobbyPut(r)
+		case http.MethodDelete:
+			body, code = v1LobbyDelete(r)
+		default:
+			code = http.StatusMethodNotAllowed
+		}
+
+		if body != nil {
+			if _, err := w.Write(body); err != nil {
+				code = http.StatusInternalServerError
+			}
+		}
+		if code >= 300 {
+			http.Error(w, http.StatusText(code), code)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
