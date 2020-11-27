3ea1265c9d7a386e9dbb738f205069c5cc67ab73
Ryan Roden-Corrent <ryan@rcorre.net>
2020-11-26 22:31:04 -0500

    Authenticate itch user via JWT.

--- main.go
+++ main.go
@@ -1,40 +1,136 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"strings"
 )
 
-var lock sync.Mutex
-var wait chan string
-var host string
+const itchURL = "https://itch.io/api/1/jwt/me"
 
-func v1GetMatch(w http.ResponseWrite, r *http.Request) {
-	lock.Lock()
+var leaderboards []v1LeaderboardEntry
 
-	if wait == nil {
-		// no other player, become a host and wait
-		host = r.RemoteAddr
-		wait = make(chan string)
-		lock.Unlock()
+type itchUser struct {
+	Username    string
+	DisplayName string
+	ID          int
+}
 
-		guest := <-wait
-	} else {
-		// send guest address to the host
-		wait <- r.RemoteAddr
-		wait = nil
-		h := host
-		host = nil
-		lock.Unlock()
+func (u *itchUser) Name() string {
+	if u.DisplayName != "" {
+		return u.DisplayName
+	}
+	return u.Username
+}
 
-		// send host address to the guest
-		w.W:
+type v1LeaderboardEntry struct {
+	UserID   int `json:"-"`
+	UserName string
+	Level    string
+	Time     float32
+}
+
+func unmarshal(r io.Reader, out interface{}) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(b, &out); err != nil {
+		return fmt.Errorf("Failed to unmarshal %q: %v", b, err)
+	}
+	return nil
+}
+
+func getItchUser(auth string) (*itchUser, error) {
+	if !strings.HasPrefix(auth, "Bearer ") {
+		return nil, fmt.Errorf("Missing bearer in %q", auth)
 	}
 
-	player = r.RemoteAddr
-	return nil, http.StatusOK
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", itchURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequest failed: %v", err)
+	}
+	req.Header.Add("Authorization", auth)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("client.Do failed: %v", err)
+	}
+
+	var response struct{ User itchUser }
+	if err := unmarshal(resp.Body, &response); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal itch response: %v", err)
+	}
+
+	return &response.User, nil
+}
+
+func v1GetLeaderboards(w http.ResponseWriter, r *http.Request) {
+	if _, err := getItchUser(r.Header.Get("Authorization")); err != nil {
+		log.Printf("Failed to lookup user: %v", err)
+		http.Error(w, "", http.StatusUnauthorized)
+		return
+	}
+
+	resp, err := json.Marshal(leaderboards)
+	if err != nil {
+		log.Printf("Failed to marshal response: %v", err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(resp); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
+}
+
+func v1PostLeaderboards(w http.ResponseWriter, r *http.Request) {
+	user, err := getItchUser(r.Header.Get("Authorization"))
+	if err != nil {
+		log.Printf("Failed to lookup user: %v", err)
+		http.Error(w, "", http.StatusUnauthorized)
+		return
+	}
+
+	var entry v1LeaderboardEntry
+	if err := unmarshal(r.Body, &entry); err != nil {
+		log.Printf("Failed to parse POST body: %v", err)
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+	if entry.Level == "" || entry.Time <= 0 {
+		log.Println("Missing time or level")
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	entry.UserName = user.Name()
+	entry.UserID = user.ID
+	leaderboards = append(leaderboards, entry)
+	w.WriteHeader(http.StatusOK)
+}
+
+func newV1API() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/leaderboards", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			v1GetLeaderboards(w, r)
+		} else if r.Method == http.MethodPost {
+			v1PostLeaderboards(w, r)
+		} else {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		}
+	})
+
+	return mux
 }
 
 func main() {
@@ -44,16 +140,9 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/v1/match", func(w http.ResponseWriter, r *http.Request) {
-		var body []byte
-		var code int
-
-		if r.Method == http.MethodGet {
-			v1GetMatch(w, r)
-		} else {
-			http.Error(w, http.StatusMethodNotAllowed)
-		}
-	})
-
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	server := &http.Server{
+		Handler: newV1API(),
+		Addr:    ":" + port,
+	}
+	log.Fatal(server.ListenAndServe())
 }
